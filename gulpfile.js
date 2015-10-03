37c95aef070cdcb7380178487f017b327b1997d9
Jesse Hughes <revelstokesnowboard@gmail.com>
2015-10-03 16:23:10 +0000

    Add gulp.

---
+++ gulpfile.js
@@ -0,0 +1,24 @@
+var gulp = require("gulp");
+
+// Don't break on errors
+var plumber = require("gulp-plumber");
+
+// Watch directory for changes
+var watch = require("gulp-watch");
+
+// ES6 compiler and Sourcemaps
+var babel = require("gulp-babel");
+var sourcemaps = require("gulp-sourcemaps");
+
+gulp.task("default", function () {
+
+  return gulp.src("src/**/*.js")
+    .pipe(plumber())
+    .pipe(watch("src/**/*.js"))
+    .pipe(sourcemaps.init())
+    .pipe(babel())
+    .pipe(concat("all.js"))
+    .pipe(sourcemaps.write("."))
+    .pipe(gulp.dest("lib/"));
+
+});
