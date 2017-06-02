e3d3daee45b5ba243ffe62f7da925b7d495fac37
Jesse Hughes <revelstokesnowboard@gmail.com>
2017-06-01 17:14:28 -0700

    Remove postinstall.js

--- postinstall.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-let fs = require('fs');
-let path = require('path');
-
-let nslogPath = path.win32.resolve('node_modules\\nslog\\build\\Release\\nslog.node');
-
-fs.unlink(nslogPath, () => console.log('removed nslog.node') );
